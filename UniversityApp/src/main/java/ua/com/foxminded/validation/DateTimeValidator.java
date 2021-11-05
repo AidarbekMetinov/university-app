73424376af753d86c40e81baaa419e179e36c36f
AidarbekMetinov <80308175+AidarbekMetinov@users.noreply.github.com>
2021-11-05 12:24:46 +0600

    first commit

---
+++ UniversityApp/src/main/java/ua/com/foxminded/validation/DateTimeValidator.java
@@ -0,0 +1,26 @@
+package ua.com.foxminded.validation;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class DateTimeValidator implements ConstraintValidator<ValidDateTime, String> {
+
+	private Pattern pattern;
+	private Matcher matcher;
+	private static final String DATE_TIME_PATTERN = "^\\d{4}[-]\\d{2}[-]\\d{2}[T]\\d{2}[:]\\d{2}[:]\\d{2}$";
+
+	@Override
+	public boolean isValid(final String dateTime, final ConstraintValidatorContext context) {
+		pattern = Pattern.compile(DATE_TIME_PATTERN);
+		if (dateTime == null) {
+			return false;
+		}
+
+		matcher = pattern.matcher(dateTime);
+		return matcher.matches();
+	}
+
+}
